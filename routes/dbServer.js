eac7b3519337853b3a6d919567ad888f3497bf55
owldamon <damonowl@gmail.com>
2017-11-02 15:47:31 +0800

    修改端口

--- routes/dbServer.js
+++
@@ -1,7 +0,0 @@
-// const mongoose = require('mongoose')
-// mongoose.connect('mongodb://localhost:27017/spa');
-// var db = mongoose.connection;
-// //db.on('error', console.error.bind(console, 'connection error:'));
-// //db.once('open', function() {
-// //  console.log("we're connected!");
-// //});
