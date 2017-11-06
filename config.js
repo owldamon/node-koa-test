5cc14a6d2a21516cf2bfd9d6d83998edd049f740
owldamon <damonowl@gmail.com>
2017-11-06 18:03:09 +0800

    日常更新

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'userinfo', // 使用哪个数据库
+    username: 'root', // 用户名
+    password: 'damonowl123', // 口令
+    host: '120.78.178.216', // 主机名
+    port: 3306 // 端口号，MySQL默认3306
+};
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
+const Pet = require('./dbServer');
 
-router.get('/', async (ctx, next) => {
+router.get('/', async(ctx, next) => {
+  
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
 })
 
-router.get('/string', async (ctx, next) => {
+router.get('/string', async(ctx, next) => {
   ctx.body = 'koa2 string'
 })
 
-router.get('/json', async (ctx, next) => {
+router.get('/json', async(ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   }
